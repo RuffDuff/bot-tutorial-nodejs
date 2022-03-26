a55ccd79988c1c85ddb08d1ecbdd33e20a925c0f
RuffDuff <grahamrivet678@gmail.com>
2022-03-26 19:24:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = JSON.parse();
+  botResponse = JSON.parse(text);
 
   options = {
     hostname: 'api.groupme.com',

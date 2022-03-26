d6fe41a93a0a83ba5870e55fbbdf9a56063339f3
RuffDuff <grahamrivet678@gmail.com>
2022-03-26 18:57:42 -0400

    repeat message?

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = request.text;
 
   options = {
     hostname: 'api.groupme.com',

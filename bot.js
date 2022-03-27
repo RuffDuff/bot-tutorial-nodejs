c530e5e8b4a3083187a37b501ea5b0f9b3eb59c9
RuffDuff <grahamrivet678@gmail.com>
2022-03-26 21:32:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 const message = JSON.parse();
 
 function outputMessage() {
-	console.log(messageValue[1].data.subject.text);
+	console.log(this[1].data.subject.text);
 }
 
 
